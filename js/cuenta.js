abafa47661bd2b25683972f3aeffa3d8a2bc6a3e
Antonio Sepúlveda <antoniosepulveda@uma.es>
2022-05-24 22:36:22 +0200

    cuenta commit

---
+++ js/cuenta.js
@@ -0,0 +1,18 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDoeC-9JGgopEOqu_ePPBDqaeet8iNhRPQ",
+    authDomain: "iusportsapp.firebaseapp.com",
+    projectId: "iusportsapp",
+    storageBucket: "iusportsapp.appspot.com",
+    messagingSenderId: "419876832949",
+    appId: "1:419876832949:web:ad23b2f7289f261c0420fc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
